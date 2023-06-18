03f3ffb5a5a556f419ccc13de08e2440bf525549
Lucas-Bechtlufft <luks_dsk@hotmail.com>
2023-06-18 19:46:27 -0300

    adicioanando relação many to many, confirmação de presença

--- hdcevents/database/migrations/2023_06_18_223334_create_event_user_table.php
+++ hdcevents/database/migrations/2023_06_18_223334_create_event_user_table.php
@@ -4,24 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateEventUserTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('event_user', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('event_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('event_user');
     }
-};
+}
